46db20c360b2f413d06d54b6ec3270cddba747bb
gusocasacreativa <134549514+gusocasacreativa@users.noreply.github.com>
2023-05-26 17:47:47 -0600

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var imprimirContratoButton = document.getElementById('imprimirContrato');
-    imprimirContratoButton.addEventListener('click', function() {
-      chrome.tabs.create({ url: '"C:\Users\gvive\OneDrive\Escritorio\extension2\contratofinal.html"' });
-    });
-  });
-  
